8dab3eb882287b2f768101fa0b6fee38ddb0a301
Tate Stone <tatejamesstone@gmail.com>
2019-09-18 16:17:56 -0600

    Styling

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -13,15 +13,26 @@ export default class Dashboard extends Component{
     render(){
         return(
             <div className="Dashboard">
-                Dashboard
-                    <Link
-                    to='/wizard'>
-                        <button 
-                            className="addnewhouse">
-                            Add New Property
-                        </button>
-                    </Link>
-                <House />
+                <div className="dashboardcontainer">
+                    <div className="dashboardminicontainer">
+                        <p
+                        className="dashboardtitle"
+                        >Dashboard</p>
+                        <Link
+                        to='/wizard'>
+                            <button 
+                                className="addnewhouse">
+                                Add New Property
+                            </button>
+                        </Link>
+                        
+                    </div>
+                    <div className="homelisting">
+                        <h3>Home Listings</h3>
+                    </div>
+                    <House />
+                    
+                </div>
             </div>
         )
     }
